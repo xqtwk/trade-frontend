d52c30ea21c35bf719daf73f0e98f225af2fdcb0
Richard <xqtwkk@gmail.com>
2023-12-02 20:31:39 +0300

    idk lol

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,15 @@ import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {provideNgxStripe} from "ngx-stripe";
 import {loggerInterceptor} from "./interceptors/logger.interceptor";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([
-      loggerInterceptor
+        loggerInterceptor
     ])),
-    provideNgxStripe('pk_test_51OIFQQBurQqw5g3lwqIQDciVWyTEhWIWlaYAyyaJdgiBHVY5BHduSL53ygobg41OZlX0vsvQXOgWQPs56b3Bkqle00UfVdt3wR')
-  ]
+    provideNgxStripe('pk_test_51OIFQQBurQqw5g3lwqIQDciVWyTEhWIWlaYAyyaJdgiBHVY5BHduSL53ygobg41OZlX0vsvQXOgWQPs56b3Bkqle00UfVdt3wR'),
+    provideAnimations()
+]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,16 @@ import {GoodsComponent} from "./components/goods/goods.component";
 import {CatalogComponent} from "./components/catalog/catalog.component";
 import {MessagesComponent} from "./components/messages/messages.component";
 import {ProfileComponent} from "./components/profile/profile.component";
-import {ProfileSettingsComponent} from "./components/profileSettings/profileSettings.component";
+import {ProfileSettingsComponent} from "./components/profile/profileSettings/profileSettings.component";
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {LoginComponent} from "./components/login/login.component";
-import {authGuard} from "./services/auth/auth.guard";
-import {unauthGuard} from "./services/auth/unauth.guard";
+import {authGuard} from "./services/guard/auth.guard";
+import {unauthGuard} from "./services/guard/unauth.guard";
 import {BalanceComponent} from "./components/balance/balance.component";
+import {httpsGuard} from "./services/guard/https.guard";
 
 export const routes: Routes = [
-  {path: '', component: MainComponent},
+  {path: '', component: MainComponent, canActivate: [httpsGuard]},
   {path: 'orders/purchases', component: PurchasesComponent, canActivate: [authGuard]},
   {path: 'orders/sales', component: SalesComponent, canActivate: [authGuard]},
   {path: 'goods', component: GoodsComponent, canActivate: [authGuard]},
